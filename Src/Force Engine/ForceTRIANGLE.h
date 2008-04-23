a88ab9b91d7e11afff9423605376175ba66d08d0
gersurfer <gersurfer@a82ef5f6-654a-0410-99b4-23d68ab79cb1>
2008-04-23 01:09:22 +0000

    ARREGLAR CONSTRUCTOR ERROR LOCO DE UNRESOLVED EXTERNAL

---
+++ Src/Force Engine/ForceTRIANGLE.h
@@ -0,0 +1,24 @@
+/****************************************************************************
+
+Force Engine v0.1
+
+Creado: 28/03/08
+Clase: ForceTRIANGLE.h
+Hecho by: German Battiston AKA Melkor
+
+****************************************************************************/
+
+//---------------------------------------------------------------------------
+
+#ifndef FORCETRIANGLE_H
+#define FORCETRIANGLE_H
+
+class ForceBOX
+{
+	ForceTRIANGLE();
+	~ForceTRIANGLE();
+};
+
+#endif /*--- FORCETRIANGLE_H ---*/
+
+//---------------------------------------------------------------------------
